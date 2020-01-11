791fc7806c92089ab2fb883641995c83fb1d18e1
Jerome <jt.bhoodz@gmail.com>
2020-01-11 23:41:42 +1300

    Log npm global versions

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 npm install -g serverless
 npm install serverless-offline serverless-dotenv-plugin
 
-mkdir -p -v /$CODEBUILD_SRC_DIR/$CODEBUILD_SRC_DIR/artifacts/$env
-cp -r -v $CODEBUILD_SRC_DIR/artifacts/$env/* $CODEBUILD_SRC_DIR/$CODEBUILD_SRC_DIR/artifacts/$env
+npm list -g --depth=0
+mkdir -p -v $CODEBUILD_SRC_DIR$CODEBUILD_SRC_DIR/artifacts/$env
+cp -r -v $CODEBUILD_SRC_DIR/artifacts/$env/* $CODEBUILD_SRC_DIR$CODEBUILD_SRC_DIR/artifacts/$env
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
