82ecc1797a8645f99c2cdfd0b67403b155a8914f
Jerome <jt.bhoodz@gmail.com>
2020-01-11 22:59:06 +1300

    Change the deploy script again to fix pipeline

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ npm install -g serverless
 npm install serverless-offline serverless-dotenv-plugin
 
 mkdir -p .serverless
-cp -r -v $CODEBUILD_SRC_DIR/artifacts/$env/* $PWD/.serverless
-serverless deploy --stage $env --package $PWD/.serverless -v
+cp -r -v $CODEBUILD_SRC_DIR/artifacts/$env/* /serverless
+serverless deploy --stage $env --package /serverless -v
